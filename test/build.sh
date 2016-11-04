e5732c6aec713391fced3976c42b0a492387b89c
Chris McKenzie <kristopolous@yahoo.com>
2016-11-03 19:22:04 -0700

    Modifying the title scraper to be city/zip based

--- test/build.sh
+++ test/build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-
 [ -e rawhtml ] && rm rawhtml
 
-for i in `seq 0 100 2000`; do
-  curl 'http://losangeles.craigslist.org/search/cto?s='$i'&max_auto_miles=170000&min_auto_year=2000&min_price=4000&postal=90034&search_distance=40' >> rawhtml
-done 
+grab() {
+  city=$1
+  zip=$2
+  echo -n "Grabbing $city $zip..."
+  for i in `seq 0 100 2000`; do
+    echo -n $i...
+    curl -s 'http://'$city'.craigslist.org/search/cto?s='$i'&max_auto_miles=170000&min_auto_year=2000&min_price=4000&postal='$zip'&search_distance=40' >> rawhtml
+  done 
+  echo ''
+}
+
+grab losangeles 90034
+grab sacramento 94203
+grab seattle 98101
 
 cat rawhtml | grep -Po '(?<=result-title hdrlnk">)([^<]*)' >> title-list.txt
 

f66da28637000a7b466bf3e0ad000bd7b6f2a21a
Chris McKenzie <kristopolous@yahoo.com>
2016-11-03 19:15:19 -0700

    Printing out fail/pass percent

--- test/run-test.php
+++ test/run-test.php
@@ -10,15 +10,20 @@ function fail($what) {
 }
 
 function success($raw, $parsed) {
-  echo "$raw " . implode(', ', array_values($parsed));
+  echo implode(', ', array_values($parsed)) . " || " . $raw;
 }
 
+$cnt = 0;
+$fail = 0;
 foreach($titleList as $title) {
   $clean = title_clean($title);
-  if(!$clean) {
+  if(!$clean || !$clean['make']) {
+    $fail ++;
     fail($title);
   } else {
-    success($title, $clean);
+    //success($title, $clean);
   }
+  $cnt++;
 }
 
+echo round(100 - (100 * $fail/$cnt)) . "% pass";

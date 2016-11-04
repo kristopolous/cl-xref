0ec8e7c9287034ccb7404b68ad5b7ffe0ce6df26
Chris McKenzie <kristopolous@yahoo.com>
2016-11-03 17:55:13 -0700

    fleshing out the plugin

--- browser/plugin.js
+++ browser/plugin.js
@@ -1,17 +1,38 @@
 
 var uniq = 'WQJL3D1oTHaEQjW0oNpPnA';
 
-"<style>." + uniq + " {
-  border: 1px solid silver;
-  display: inline-block;
-}</style>";
+"<style>
 
-var btn = document.createElement('span');
-btn.className = uniq;
-btn.innerHTML = 'Get info';
-btn.onclick = function() {
-  getInfo(btn, TBD);
+
+function getInfo(el, data) {
 }
-TBD.appendChild(btn);
 
-'<span class="' + uniq + '">Get info</span>';
+function inject() {
+  var style = document.createElement('style');
+  style.innerHTML = [
+    '.' + uniq + ' {',
+    '  border: 1px solid silver;'
+    '  display: inline-block;',
+    '}'
+  ].implode('');
+  document.body.appendChild(style);
+
+  var allTitles = document.querySelectorAll('.result-title');
+
+  allTitles.forEach(function(what) {
+    var 
+      title = what.innerHTML,
+      btn = document.createElement('span'),
+      container = what.parentNode;
+
+    btn.className = uniq;
+    btn.innerHTML = 'Get info';
+    btn.onclick = function() {
+      getInfo(btn, title);
+    }
+    container.appendChild(btn);
+  });
+}
+
+inject();
+

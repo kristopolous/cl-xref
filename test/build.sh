e005b54b7a527625e28643d2c76872f87f5aa791
Chris McKenzie <kristopolous@yahoo.com>
2016-11-03 19:13:41 -0700

    increasing range

--- test/build.sh
+++ test/build.sh
@@ -3,8 +3,8 @@
 
 [ -e rawhtml ] && rm rawhtml
 
-for i in `seq 0 100 1000`; do
-  curl 'http://losangeles.craigslist.org/search/cto?s='$i'&max_auto_miles=170000&min_auto_year=2007&min_price=4000&postal=90034&search_distance=30' >> rawhtml
+for i in `seq 0 100 2000`; do
+  curl 'http://losangeles.craigslist.org/search/cto?s='$i'&max_auto_miles=170000&min_auto_year=2000&min_price=4000&postal=90034&search_distance=40' >> rawhtml
 done 
 
 car rawhtml | grep -Po '(?<=result-title hdrlnk">)([^<]*)' >> title-list.txt

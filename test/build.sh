8f0c31127fbd45a1152dbd532c086ab8d8812e72
Chris McKenzie <kristopolous@yahoo.com>
2016-11-03 19:15:09 -0700

    Fixing a typo

--- test/build.sh
+++ test/build.sh
@@ -7,7 +7,7 @@ for i in `seq 0 100 2000`; do
   curl 'http://losangeles.craigslist.org/search/cto?s='$i'&max_auto_miles=170000&min_auto_year=2000&min_price=4000&postal=90034&search_distance=40' >> rawhtml
 done 
 
-car rawhtml | grep -Po '(?<=result-title hdrlnk">)([^<]*)' >> title-list.txt
+cat rawhtml | grep -Po '(?<=result-title hdrlnk">)([^<]*)' >> title-list.txt
 
 cat title-list.txt | sort | uniq > tmp
 mv tmp title-list.txt

474acd9d317e6e0d035fe1293ef6d0b413906871
Chris McKenzie <kristopolous@yahoo.com>
2016-11-03 18:31:46 -0700

    saving raw html to check the initial title grabbing

--- test/build.sh
+++ test/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 
+[ -e rawhtml ] && rm rawhtml
+
 for i in `seq 0 100 1000`; do
-  curl 'http://losangeles.craigslist.org/search/cto?s='$i'&max_auto_miles=170000&min_auto_year=2007&min_price=4000&postal=90034&search_distance=30' \
-    | grep -Po '(?<=result-title hdrlnk">)([^<]*)' >> title-list.txt
-done
+  curl 'http://losangeles.craigslist.org/search/cto?s='$i'&max_auto_miles=170000&min_auto_year=2007&min_price=4000&postal=90034&search_distance=30' >> rawhtml
+done 
+
+car rawhtml | grep -Po '(?<=result-title hdrlnk">)([^<]*)' >> title-list.txt
 
 cat title-list.txt | sort | uniq > tmp
 mv tmp title-list.txt

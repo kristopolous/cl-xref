20d60a6ec9fd463bcbd4ec090ae2691f6ee63eb3
Chris McKenzie <kristopolous@yahoo.com>
2016-11-05 16:45:58 -0700

    Separating the two systems to externalize the lib

---
+++ server/lib.php
@@ -0,0 +1,45 @@
+<?php
+
+include('title-clean.php');
+
+function secrets($what) {
+  $map = parse_ini_file('secrets.ini');
+  if(isset($map[$what])) { 
+    return $map[$what];
+  }
+}
+
+function edmunds($ep, $query) {
+  $key = secrets('edmunds');
+  $url = implode('', [
+    'https://api.edmunds.com/v1/api/tmv/tmvservice/',
+    $ep, '?',
+    http_build_query(array_merge($query, [
+      'fmt' => 'json',
+      'api_key' => $key
+    ])
+  ]);
+  return file_get_contents($url);
+}
+
+function kbb($ep, $query) {
+  $key = secrets('kbb');
+}
+
+function price() {
+  return edmunds('calculatetypicallyequippedusedtmv', [
+    'zip' => $_REQUEST['zip'],
+    'styleid' => 123
+  ]);
+}
+
+function rating() {
+}
+
+function all() {
+  return [
+    'price' => price(),
+    'rating' => rating()
+  ];
+}
+

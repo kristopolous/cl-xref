3c4a822614e6cfc78aecb09d953d8e4a02ee4724
Chris McKenzie <kristopolous@yahoo.com>
2016-11-04 18:39:20 -0700

    normalization of makes

--- db/merge.sql
+++ db/merge.sql
@@ -1,2 +1,4 @@
 alter table VehicleModelYear add column mushed text;
 update VehicleModelYear set mushed = replace(make || model, ' ', '');
+create table make('id' INTEGER PRIMARY KEY, 'make' TEXT NULL)
+

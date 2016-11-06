8b3d3fa1d111a767a38e1dafe322a928cb3963cf
Chris McKenzie <kristopolous@yahoo.com>
2016-11-05 17:00:29 -0700

    Adding a few more cities in

--- test/build.sh
+++ test/build.sh
@@ -8,7 +8,7 @@ grab() {
   echo -n "Grabbing $city $zip..."
   for i in `seq 0 100 2000`; do
     echo -n $i...
-    curl -s 'http://'$city'.craigslist.org/search/cto?s='$i'&max_auto_miles=170000&min_auto_year=2000&min_price=4000&postal='$zip'&search_distance=40' >> rawhtml
+    curl -s 'http://'$city'.craigslist.org/search/cto?s='$i'&min_auto_year=2000&min_price=4000&postal='$zip'&search_distance=40' >> rawhtml
   done 
   echo ''
 }
@@ -16,6 +16,9 @@ grab() {
 grab losangeles 90034
 grab sacramento 94203
 grab seattle 98101
+grab chicago 60290
+grab houston 77001
+grab philadelphia 19019
 
 cat rawhtml | grep -Po '(?<=result-title hdrlnk">)([^<]*)' >> title-list.txt
 
